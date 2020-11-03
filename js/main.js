c51a407d803ff6363857ce1353bdd2add6eed33a
NANNADA4 <68773516+NANNADA4@users.noreply.github.com>
2020-11-03 15:18:00 +0900

    change class name Sneaker to EntryImage

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 //Items
 const title = document.querySelector(".title");
-const sneaker = document.querySelector(".sneaker img");
+const EntryImage = document.querySelector(".EntryImage img");
 const MovePage = document.querySelector(".MovePage");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
@@ -19,7 +19,7 @@ container.addEventListener("mouseenter", (e) => {
     card.style.transition = "none";
     //Popout
     title.style.transform = "translateZ(150px)";
-    sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+    EntryImage.style.transform = "translateZ(200px) rotateZ(-45deg)";
     description.style.transform = "translateZ(125px)";
     sizes.style.transform = "translateZ(100px)";
     MovePage.style.transform = "translateZ(75px)";
@@ -30,7 +30,7 @@ container.addEventListener("mouseleave", (e) => {
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     //Popback
     title.style.transform = "translateZ(0px)";
-    sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+    EntryImage.style.transform = "translateZ(0px) rotateZ(0deg)";
     description.style.transform = "translateZ(0px)";
     sizes.style.transform = "translateZ(0px)";
     MovePage.style.transform = "translateZ(0px)";
