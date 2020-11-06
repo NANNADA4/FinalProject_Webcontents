e229b83159db8389e9f2781ec8e712264d454948
NANNADA4 <68773516+NANNADA4@users.noreply.github.com>
2020-11-07 05:32:00 +0900

    delete container

--- js/entry.js
+++ js/entry.js
@@ -1,31 +1,28 @@
 //Movement Animation to happen
 const card = document.querySelector(".card");
-const container = document.querySelector(".container");
 //Items
 const title = document.querySelector(".title");
 const EntryImage = document.querySelector(".EntryImage img");
 const MovePage = document.querySelector(".MovePage");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
-
 //Moving Animation Event
-container.addEventListener("mousemove", (e) => {
+card.addEventListener("mousemove", (e) => {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
     let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //Animate In
-container.addEventListener("mouseenter", (e) => {
+card.addEventListener("mouseenter", (e) => {
     card.style.transition = "none";
     //Popout
     title.style.transform = "translateZ(150px)";
     EntryImage.style.transform = "translateZ(200px) rotateZ(-45deg)";
     description.style.transform = "translateZ(125px)";
     sizes.style.transform = "translateZ(100px)";
-    MovePage.style.transform = "translateZ(75px)";
 });
 //Animate Out
-container.addEventListener("mouseleave", (e) => {
+card.addEventListener("mouseleave", (e) => {
     card.style.transition = "all 0.5s ease";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     //Popback
@@ -33,7 +30,6 @@ container.addEventListener("mouseleave", (e) => {
     EntryImage.style.transform = "translateZ(0px) rotateZ(0deg)";
     description.style.transform = "translateZ(0px)";
     sizes.style.transform = "translateZ(0px)";
-    MovePage.style.transform = "translateZ(0px)";
 });
 //EntryTitle onmouse event
 function ClickTitle1(t, o) {
@@ -56,7 +52,6 @@ function ClickTitle5(t, o) {
     var o = document.getElementById(o);
     o.innerHTML = t;
 }
-
 //Page move with button click
 function dbl_click(href) {
     location.href = href;
