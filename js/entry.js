9350ced05cb637f60d73a7c6369dbf3ebe39e340
NANNADA4 <coco96062233@gmail.com>
2020-11-10 14:44:22 +0900

    Delete All from javascript except image rotation and image slider

--- js/entry.js
+++ js/entry.js
@@ -1,35 +1,12 @@
-//Movement Animation to happen
+// mouseenter시 EntryImage 회전
 const card = document.querySelector(".card");
-//Items
-const title = document.querySelector(".title");
 const EntryImage = document.querySelector(".EntryImage img");
-const MovePage = document.querySelector(".MovePage");
-const description = document.querySelector(".info h3");
-const sizes = document.querySelector(".sizes");
-//Moving Animation Event
-card.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
-//Animate In
+
 card.addEventListener("mouseenter", (e) => {
-    card.style.transition = "none";
-    //Popout
-    title.style.transform = "translateZ(150px)";
-    EntryImage.style.transform = "translateZ(200px) rotateZ(-45deg)";
-    description.style.transform = "translateZ(125px)";
-    sizes.style.transform = "translateZ(100px)";
+    EntryImage.style.transform = "rotateZ(-45deg)";
 });
-//Animate Out
 card.addEventListener("mouseleave", (e) => {
-    card.style.transition = "all 0.5s ease";
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    //Popback
-    title.style.transform = "translateZ(0px)";
-    EntryImage.style.transform = "translateZ(0px) rotateZ(0deg)";
-    description.style.transform = "translateZ(0px)";
-    sizes.style.transform = "translateZ(0px)";
+    EntryImage.style.transform = "rotateZ(0deg)";
 });
 //EntryTitle onmouse event
 function ClickTitle1(t, o) {
