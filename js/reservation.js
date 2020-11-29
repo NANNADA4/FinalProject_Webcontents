893f66646217882ba0ef2bd6b492ee8db46cebb9
NANNADA4 <coco96062233@gmail.com>
2020-11-30 04:52:12 +0900

    Add formsubmit()

--- js/reservation.js
+++ js/reservation.js
@@ -15,3 +15,19 @@ function printTime() {
 window.onload = function () {
     printTime();
 };
+
+function formsubmit() {
+    if (!document.form1.date.value) {
+        alert("날짜를 입력해주세요.");
+    } else if (!document.form1.time.value) {
+        alert("시간을 입력해주세요.");
+    } else if (!document.form1.text.value) {
+        alert("전화번호를 입력해주세요.");
+    } else if (!document.form1.number.value) {
+        alert("인원 수를 입력해주세요.");
+    } else if (!document.form1.email.value) {
+        alert("이메일을 입력해주세요.");
+    } else {
+        return false;
+    }
+}
