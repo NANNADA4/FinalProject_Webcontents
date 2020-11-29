a740eb83013d439ebbbe90e97185e704ce802609
NANNADA4 <coco96062233@gmail.com>
2020-11-30 08:26:42 +0900

    Add mailto, Finish All!!!!!!!!!!!!

--- js/reservation.js
+++ js/reservation.js
@@ -23,18 +23,43 @@ function check() {
     var numberValue = document.getElementById("number1").value;
     var emailValue = document.getElementById("email1").value;
 
-    confirm(
-        dateValue +
-            "일, " +
+    var confirm_check = confirm(
+        "예약 날짜 : " +
+            dateValue +
+            "일, 예약 시간 : " +
             timeValue +
-            "시, 전화번호 : " +
+            "분, 전화번호 : " +
             textValue +
-            ", " +
+            ", 인원수 : " +
             numberValue +
             "명, 이메일 : " +
             emailValue +
             "이 맞나요?"
     );
+    if (confirm_check == true) {
+        alert("!!!하단의 예약 확인 버튼을 눌러주셔야 예약이 됩니다!!!");
+        var MailSend = document.getElementById("sendMail");
+        MailSend.innerHTML =
+            "<a href='mailto:coco96062233@gmail.com," +
+            emailValue +
+            "?subject=[예약] " +
+            dateValue +
+            " " +
+            timeValue +
+            " " +
+            numberValue +
+            "명 &body= 더덕향을 이용해주셔서 감사합니다. 본 이메일은 예약 임시확인 이메일입니다. %0D%0A 일시 : " +
+            dateValue +
+            " " +
+            timeValue +
+            "%0D%0A 인원 수 : " +
+            numberValue +
+            "%0D%0A 휴대전화 번호 : " +
+            textValue +
+            "%0D%0A 문의전화 : 033-335-6888 더덕향'>예약 확인</a>";
+    } else if (confirm_check == false) {
+        alert("예약이 취소됩니다.");
+    }
 }
 
 function formsubmit() {
